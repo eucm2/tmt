b836a9e7a60d6c0c0a2cb33b1c5609a4b6de7a96
eugenio <eugenio@eugenio-H81M-H>
2019-04-03 15:17:37 -0600

    tabla de usurioa en kindomlikes para poder seleccionar uno u otro

--- src/main/java/com/mycompany/mavenproject1/kingdomlikes.java
+++ src/main/java/com/mycompany/mavenproject1/kingdomlikes.java
@@ -27,6 +27,7 @@ public class kingdomlikes extends javax.swing.JInternalFrame {
     Statement statement = null;
     String path_drive = "";
     cControl c = new cControl();
+    DefaultTableModel modelo=new DefaultTableModel();
 
     //VARIABLE QUE DETECTA SI SE CAMBIO EL TEXTO Y HACE UN UPDATE
     private Connection connect() {
@@ -48,6 +49,14 @@ public class kingdomlikes extends javax.swing.JInternalFrame {
     //CARGADOR
     public kingdomlikes() {
         initComponents();
+        //CARGAMOS EL MODELO DE LA TABLA PUBLICACIONES
+        modelo.addColumn("id");
+        modelo.addColumn("user");
+        modelo.addColumn("password");
+        modelo.addColumn("redSocal");
+        modelo.addColumn("activo");
+        //CARGAMOS LA TABLA DE PUBLICACIONES
+        carga_tabla();
 
         try {
             connection = DriverManager.getConnection("jdbc:sqlite:tmt.db");
@@ -133,6 +142,9 @@ public class kingdomlikes extends javax.swing.JInternalFrame {
         jLabel17 = new javax.swing.JLabel();
         txtCadaVideos = new javax.swing.JTextField();
         jLabel18 = new javax.swing.JLabel();
+        jScrollPane2 = new javax.swing.JScrollPane();
+        tabla_accesos = new javax.swing.JTable();
+        jLabel2 = new javax.swing.JLabel();
 
         setTitle("Kingdomlikes");
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
@@ -144,10 +156,10 @@ public class kingdomlikes extends javax.swing.JInternalFrame {
                 btnGanarPuntosActionPerformed(evt);
             }
         });
-        getContentPane().add(btnGanarPuntos, new org.netbeans.lib.awtextra.AbsoluteConstraints(490, 70, -1, -1));
+        getContentPane().add(btnGanarPuntos, new org.netbeans.lib.awtextra.AbsoluteConstraints(490, 250, -1, -1));
 
         jLabel14.setText("Paises");
-        getContentPane().add(jLabel14, new org.netbeans.lib.awtextra.AbsoluteConstraints(290, 170, -1, -1));
+        getContentPane().add(jLabel14, new org.netbeans.lib.awtextra.AbsoluteConstraints(290, 350, -1, -1));
 
         userKL.addKeyListener(new java.awt.event.KeyAdapter() {
             public void keyPressed(java.awt.event.KeyEvent evt) {
@@ -174,7 +186,7 @@ public class kingdomlikes extends javax.swing.JInternalFrame {
                 KLnumPaginasKeyPressed(evt);
             }
         });
-        getContentPane().add(KLnumPaginas, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 190, 240, -1));
+        getContentPane().add(KLnumPaginas, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 370, 240, -1));
 
         jLabel15.setText("User KL");
         getContentPane().add(jLabel15, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 10, -1, -1));
@@ -185,20 +197,20 @@ public class kingdomlikes extends javax.swing.JInternalFrame {
                 btnKLCrearPaginasActionPerformed(evt);
             }
         });
-        getContentPane().add(btnKLCrearPaginas, new org.netbeans.lib.awtextra.AbsoluteConstraints(490, 280, -1, -1));
+        getContentPane().add(btnKLCrearPaginas, new org.netbeans.lib.awtextra.AbsoluteConstraints(490, 460, -1, -1));
 
         KLPaises.setColumns(20);
         KLPaises.setRows(5);
         KLPaises.setText("Mexico\nArgentina\nSpain\nChile\nColombia\nUnited States");
         jScrollPane1.setViewportView(KLPaises);
 
-        getContentPane().add(jScrollPane1, new org.netbeans.lib.awtextra.AbsoluteConstraints(290, 190, 130, 110));
+        getContentPane().add(jScrollPane1, new org.netbeans.lib.awtextra.AbsoluteConstraints(290, 370, 130, 110));
 
         jLabel16.setText("Numero de paginas a crear");
-        getContentPane().add(jLabel16, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 170, -1, -1));
+        getContentPane().add(jLabel16, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 350, -1, -1));
 
         faltanSegundos.setText("faltan");
-        getContentPane().add(faltanSegundos, new org.netbeans.lib.awtextra.AbsoluteConstraints(380, 70, -1, -1));
+        getContentPane().add(faltanSegundos, new org.netbeans.lib.awtextra.AbsoluteConstraints(380, 250, -1, -1));
 
         checIntercambiarCuentas.setSelected(true);
         checIntercambiarCuentas.setText("Intercambiar cuentas");
@@ -207,16 +219,45 @@ public class kingdomlikes extends javax.swing.JInternalFrame {
                 checIntercambiarCuentasActionPerformed(evt);
             }
         });
-        getContentPane().add(checIntercambiarCuentas, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 70, -1, -1));
+        getContentPane().add(checIntercambiarCuentas, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 250, -1, -1));
 
         jLabel17.setText("cada");
-        getContentPane().add(jLabel17, new org.netbeans.lib.awtextra.AbsoluteConstraints(200, 70, -1, -1));
+        getContentPane().add(jLabel17, new org.netbeans.lib.awtextra.AbsoluteConstraints(200, 250, -1, -1));
 
         txtCadaVideos.setText("15");
-        getContentPane().add(txtCadaVideos, new org.netbeans.lib.awtextra.AbsoluteConstraints(240, 70, 60, -1));
+        getContentPane().add(txtCadaVideos, new org.netbeans.lib.awtextra.AbsoluteConstraints(240, 250, 60, -1));
 
         jLabel18.setText("segundos");
-        getContentPane().add(jLabel18, new org.netbeans.lib.awtextra.AbsoluteConstraints(310, 70, -1, -1));
+        getContentPane().add(jLabel18, new org.netbeans.lib.awtextra.AbsoluteConstraints(310, 250, -1, -1));
+
+        tabla_accesos.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null, null, null},
+                {null, null, null, null, null},
+                {null, null, null, null, null},
+                {null, null, null, null, null}
+            },
+            new String [] {
+                "id", "user", "password", "redSocial", "activo"
+            }
+        ));
+        tabla_accesos.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseReleased(java.awt.event.MouseEvent evt) {
+                tabla_accesosMouseReleased(evt);
+            }
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                tabla_accesosMouseClicked(evt);
+            }
+            public void mouseEntered(java.awt.event.MouseEvent evt) {
+                tabla_accesosMouseEntered(evt);
+            }
+        });
+        jScrollPane2.setViewportView(tabla_accesos);
+
+        getContentPane().add(jScrollPane2, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 70, 600, 160));
+
+        jLabel2.setText("jLabel2");
+        getContentPane().add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(170, 600, -1, -1));
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
@@ -282,6 +323,31 @@ public class kingdomlikes extends javax.swing.JInternalFrame {
 
         intercambiarCuentas();
     }//GEN-LAST:event_checIntercambiarCuentasActionPerformed
+
+    private void tabla_accesosMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_tabla_accesosMouseClicked
+        asigna_tabla_accesos_input_text();
+    }//GEN-LAST:event_tabla_accesosMouseClicked
+
+    private void tabla_accesosMouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_tabla_accesosMouseEntered
+        asigna_tabla_accesos_input_text();
+    }//GEN-LAST:event_tabla_accesosMouseEntered
+
+    private void tabla_accesosMouseReleased(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_tabla_accesosMouseReleased
+        asigna_tabla_accesos_input_text();
+    }//GEN-LAST:event_tabla_accesosMouseReleased
+    
+    //COLOCAMOS EL TEXTO DE LA TABLA EN CADA INPUT TEXT
+    public void asigna_tabla_accesos_input_text() {
+        try {
+            String idPub = tabla_accesos.getValueAt(tabla_accesos.getSelectedRow(), 0).toString();
+            userKL.setText(tabla_accesos.getValueAt(tabla_accesos.getSelectedRow(), 1).toString());
+            passwordKL.setText(tabla_accesos.getValueAt(tabla_accesos.getSelectedRow(), 2).toString());
+        } catch (Exception e) {
+            System.err.println(e);
+        }
+    }
+
+    
     public void intercambiarCuentas(){
         if (checIntercambiarCuentas.isSelected()) {
             // Bucle infinito
@@ -334,6 +400,54 @@ public class kingdomlikes extends javax.swing.JInternalFrame {
             System.out.println(e.getMessage());
         }
     }
+    public void carga_tabla() {
+        String Dato[]=new String[5];
+        modelo.setRowCount(0);
+        try {
+            //CONECTA A LA BD
+            connection = this.connect();
+            statement = connection.createStatement();
+            statement.setQueryTimeout(20);
+            //QUERY QUE JALA TODAS LAS PUBLICACIONES
+            String query = "SELECT "
+                    + "id, "
+                    + "user, "
+                    + "password, "
+                    + "redSocal, "
+                    + "activo "
+                    + "FROM accesos "
+                    + "WHERE redSocal='KL';";
+            ResultSet rs = statement.executeQuery(query);
+            //CICLO QUE LLENA TODO EL MODELO
+            while (rs.next()) {
+                Dato[0]=rs.getString("id");
+                Dato[1]=rs.getString("user");
+                Dato[2]=rs.getString("password");
+                Dato[3]=rs.getString("redSocal");
+                Dato[4]=rs.getString("activo");
+                modelo.addRow(Dato);
+            }
+            //LLENAMOS LA TABLA CON EL MODELO
+            tabla_accesos.setModel(modelo);
+            //OCULTAMOS EL ID EN LA TABLA
+            //tabla.getColumn("id").setMaxWidth(0);
+            userKL.setText("");
+            passwordKL.setText("");
+        } catch (SQLException e) {
+            System.err.println(e.getMessage());
+        } finally {
+
+            try {
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (SQLException e) {
+                // connection close failed.
+                System.err.println(e);
+            }
+
+        }
+    }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JTextArea KLPaises;
@@ -350,8 +464,11 @@ public class kingdomlikes extends javax.swing.JInternalFrame {
     private javax.swing.JLabel jLabel16;
     private javax.swing.JLabel jLabel17;
     private javax.swing.JLabel jLabel18;
+    private javax.swing.JLabel jLabel2;
     private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JScrollPane jScrollPane2;
     private javax.swing.JPasswordField passwordKL;
+    private javax.swing.JTable tabla_accesos;
     private javax.swing.JTextField txtCadaVideos;
     private javax.swing.JTextField userKL;
     // End of variables declaration//GEN-END:variables
